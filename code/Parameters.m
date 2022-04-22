a382f873313326619a2976bee4a33934d4733051
Brian Livingston <ak.brian09@gmail.com>
2022-04-22 01:42:29 -0500

    major edits

--- code/Parameters.m
+++ code/Parameters.m
@@ -19,6 +19,9 @@ classdef Parameters
         z = [0.99,1.01]
         nz
         
+        L = [0.9,0.96]
+        
+        pi_z
         pimat
         
         dims
@@ -26,8 +29,11 @@ classdef Parameters
         maxiters = 1e4
         tol = 1e-8
         del_update = 0.4
-        
-        L = 0.96
+                
+        % Simulation
+        sim_nHH = 10000;
+        sim_tburn = 100;
+        sim_T = 1000;
     end  
     
     methods
@@ -38,12 +44,17 @@ classdef Parameters
             
             obj.z = shiftdim(obj.z,-2);
             
-            obj.pimat = [...
+            obj.pi_z = [...
+                7/8, 1/8
+                1/8, 7/8
+                ];
+            
+            obj.pimat = fliplr(flipud([...
                 0.8507, 0.1229, 7/12, 3/32
                 0.1159, 0.8361, 1/32, 7/20
                 0.0243, 0.0021, 7/24, 1/32
                 0.0091, 0.0389, 3/32, 21/40
-                ];
+                ]))';
         end
     end
 end
--- code/main.m
+++ code/main.m
@@ -1,53 +1,96 @@
 
 clear
 
+% Read parameters file
 p = Parameters();
-kgrid = create_grid(p.kmin,p.kmax,p.nk,p.kcurve);
-aggKgrid = create_grid(p.Kmin,p.Kmax,p.nK,p.Kcurve);
+
+% Create grids for household assets and aggregate capital
+grids = struct();
+grids.k = create_grid(p.kmin,p.kmax,p.nk,p.kcurve);
+grids.K = create_grid(p.Kmin,p.Kmax,p.nK,p.Kcurve);
 
 % Guess policy function k'(k,K,s,z)
-kpol0 = repmat(0.9*kgrid, [1,p.nK,p.nl,p.nz]);
+kpol0 = 0.9 * grids.k .* ones([1,p.nK,p.nl,p.nz]);
 
 % Guess capital law of motion
-lom_K = struct();
-lom_K.alpha = shiftdim([0;0],-3);
-lom_K.beta = shiftdim([1;1],-3);
+lom0_K = struct();
+lom0_K.alpha = reshape([0,0],[1,1,1,2]);
+lom0_K.beta = reshape([1,1],[1,1,1,2]);
 
-% Solve for policy function
-kpol = iterate(p,kgrid,aggKgrid,kpol0,lom_K);
+% Solve
+iterate_lom(p,grids,kpol0,lom0_K)
 
-disp('done')
-
-function kpol = iterate(p,kgrid,aggKgrid,kpol0,lom_K)
-    kpol = kpol0;
+% Iterate over law of motion
+function b = iterate_lom(p,grids,kpol0,lom0_K)
+    lom_K = lom0_K;
+    
     for i=1:p.maxiters
+        % Solve for policy function
+        kpol = solve_policy(p,grids,kpol0,lom_K);
+
+        % Simulate
+        b1 = simulate(p,grids,kpol);
+
+        % Check difference
+        b0 = [lom_K.alpha(:)';lom_K.beta(:)'];
+        bnorm = norm(b1(:)-b0(:),Inf);
+        delta = 0.99;
+        lom_K.alpha(1) = delta*b0(1,1) + (1-delta)*b1(1,1);
+        lom_K.alpha(2) = delta*b0(1,2) + (1-delta)*b1(1,2);
+        lom_K.beta(1) = delta*b0(2,1) + (1-delta)*b1(2,1);
+        lom_K.beta(2) = delta*b0(2,2) + (1-delta)*b1(2,2);
         
-        kpol_update = solve_policy(p,kgrid,aggKgrid,kpol,lom_K);
-        knorm = norm(kpol(:)-kpol_update(:),Inf);
-        kpol = kpol_update;
-        
-        if knorm < p.tol
+        if bnorm < p.tol
             disp('Converged')
             return
-        else
-            fprintf('Iteration = %d, norm=%f\n',i,knorm)
+        elseif mod(i,10)==0
+        	fprintf('Iteration = %d, norm=%f\n',i,bnorm)
         end
     end
     error('No convergence')
 end
 
-function kpol_update = solve_policy(p,kgrid,aggKgrid,kpol,lom_K)
-    % Solves the policy function given the aggregate states and the
-    % conjecture for the law of motion.
+function kpol = solve_policy(p,grids,kpol0,lom_K)
+    kpol = kpol0;
+    for i=1:p.maxiters
+        
+        kpol_update = update_policy(p,grids,kpol,lom_K);
+        knorm = norm(kpol(:)-kpol_update(:),Inf);
+        
+        % Update decision rule
+        kpol = p.del_update * kpol_update...
+            + (1-p.del_update) * kpol;
+        
+        if knorm < p.tol
+            disp('Converged')
+            return
+        elseif mod(i,50)==0
+        	fprintf('Iteration = %d, norm=%f\n',i,knorm)
+        end
+    end
+    error('No convergence')
+end
+
+function kpol_euler = update_policy(p,grids,kpol,lom_K)
+    % Updates the policy function
     
-    % Conjectured K', agg.Kp
-    Kp = capital_conjecture(lom_K,aggKgrid');
+    % Conjectured values for K'(K,z), repeated onto k and l dimensions.
+    % dim(Kpvec) = (nk*nK*nl,nz)
+    Kp = capital_conjecture(lom_K,grids.K');
     Kpvec = repmat(Kp,[p.nk,1,p.nl,1]);
     Kpvec = reshape(Kpvec,[],p.nz);
     
-    % Prices
-    [r, w] = compute_prices(p, aggKgrid');
+    % Prices r(K,z) and w(K,z)
+    % dim(r) = dim(w) = (1,nk,1,nz)
+    [r, w] = compute_prices(p, grids.K');
+    
+    % Prices r(K',z') and w(K',z')
     [rp, wp] = compute_prices(p, Kp);
+    
+    % Repeat r' and w' onto (k,l,z,l') dimensions and reshape.
+    % Rows correspond with current state and columns correspond with
+    % (l',z').
+    % dim(rp) = dim(wp) = (nk*nK*nl*nz,nl*nz)
     rp = reshape(rp,[1,p.nK,1,1,1,p.nz]);
     rp = repmat(rp,[p.nk,1,p.nl,p.nz,p.nl,1]);
     rp = reshape(rp,[],p.nl*p.nz);
@@ -55,10 +98,8 @@ function kpol_update = solve_policy(p,kgrid,aggKgrid,kpol,lom_K)
     wp = repmat(wp,[p.nk,1,p.nl,p.nz,p.nl,1]);
     wp = reshape(wp,[],p.nl*p.nz);
     
-    % Labor supply, dimension (nl,nz)
-    l = reshape(p.l, [p.nl,1]);
-    l = repmat(l,[1,p.nz]);
-    lp = l(:)';
+    % Next periods labor supply, dimension (nl,nz)
+    lp = repmat(p.l(:),[1,p.nz]);
 
     % Interpolate k'(k'(k,K,l,z),K',l',z')
     kpp = zeros([p.nk*p.nK*p.nl*p.nz,p.nl,p.nz]);
@@ -66,14 +107,14 @@ function kpol_update = solve_policy(p,kgrid,aggKgrid,kpol,lom_K)
         for iz=1:p.nz % z'
             kp_lz = kpol(:,:,il,iz);
             kinterp = griddedInterpolant(...
-                {kgrid,aggKgrid},kp_lz,'spline');
+                {grids.k,grids.K},kp_lz,'spline');
             kpp(:,il,iz) = kinterp(kpol(:),Kpvec(:));
         end
     end
     
     % Construct c' vector
     kpp = reshape(kpp,[],p.nl*p.nz);
-    cp = (1+rp+p.delta).*kpol(:) + wp.*lp - kpp;
+    cp = (1+rp+p.delta).*kpol(:) + wp.*lp(:)' - kpp;
     
     % Expected marginal utility
     A = kron(p.pimat,ones(p.nk*p.nK,1));
@@ -81,14 +122,9 @@ function kpol_update = solve_policy(p,kgrid,aggKgrid,kpol,lom_K)
     
     % k' decision rule on LHS of Euler
     eMU = reshape(eMU,p.dims);
-    kpol_euler = (1+r+p.delta).*kgrid + w.*shiftdim(l,-2) - 1./(p.beta*eMU);
+    kpol_euler = (1+r+p.delta).*grids.k + w.*shiftdim(lp,-2) - 1./(p.beta*eMU);
     kpol_euler = max(kpol_euler,p.kmin);
     kpol_euler = min(kpol_euler,p.kmax);
-    
-    
-    % Update decision rule
-    kpol_update = p.del_update * kpol_euler...
-        + (1-p.del_update) * kpol;
 end
 
 function Kp = capital_conjecture(lom_K, K)
@@ -97,7 +133,7 @@ function Kp = capital_conjecture(lom_K, K)
 end
 
 function [r, w] = compute_prices(p, K)
-    klratio = K ./ p.L;
+    klratio = K ./ reshape(p.L,[1,1,1,p.nz]);
 
     % r(K,z), dimension (1,nK,1,nz)
     r = p.z * p.alpha .* klratio .^ (p.alpha-1);
@@ -106,4 +142,88 @@ function [r, w] = compute_prices(p, K)
     % w(K,z), dimension (1,nK,1,nz)
     w = p.z * (1-p.alpha) .* klratio .^(p.alpha);
     w = reshape(w, [1,p.nK,1,p.nz]);
+end
+
+function b = simulate(p,grids,kpol)
+
+    % Create policy interpolants
+    kinterp = cell(p.nl,p.nz);
+    for il=1:p.nl % l'
+        for iz=1:p.nz % z'
+            kp_lz = kpol(:,:,il,iz);
+            kinterp{il,iz} = griddedInterpolant(...
+                {grids.k,grids.K},kp_lz,'spline');
+        end
+    end
+    
+    % Time periods to simulate
+    T = p.sim_tburn + p.sim_T;
+    
+    rng(9520);
+    
+    % Aggregate productivity: iz=1 bad, iz=2 good
+    zrand = rand(T,1);
+    iz0 = 1;
+    
+    % Initial capital distribution
+    k = linspace(p.kmin,p.kmax,p.sim_nHH)';
+    
+    % Initial employment status
+    employed = rand(p.sim_nHH,1) < p.L(iz);
+    l = (employed) * 1 + (~employed) * 2;
+    
+    % Employment transitions conditional on agg transition
+	pi_l_trans = cell(2,2);
+    pi_l_trans{1,1} = p.pimat([1,3],[1,3]);
+    pi_l_trans{1,2} = p.pimat([1,3],[2,4]);
+    pi_l_trans{2,1} = p.pimat([2,4],[1,3]);
+    pi_l_trans{2,2} = p.pimat([2,4],[2,4]);
+    
+    for i=1:2
+        for j=1:2
+            pi_l_trans{i,j} = pi_l_trans{i,j} ./ sum(pi_l_trans{i,j},2);
+            pi_l_trans{i,j} = cumsum(pi_l_trans{i,j},2);
+        end
+    end
+    
+    K_t = zeros(T,1);
+    z_t = zeros(T,1);
+    for t=1:T
+        K_t(t) = mean(k);
+        z_t(t) = iz0;
+
+        % Update next aggregate productivity
+        iz1 = 1 + (zrand(t) < p.pi_z(1,iz0));
+                
+        % Draw idiosyncratic shocks
+        lrand = rand(p.sim_nHH,1);
+        [~,l] = min(lrand>pi_l_trans{iz0,iz1}(l,:),[],2);
+        
+        % Interpolate
+        Kvec = K_t(t) * ones(p.sim_nHH,1);
+        
+        employed = (l==1);
+        k(~employed) = kinterp{1,iz0}(k(~employed),Kvec(~employed));
+        k(employed) = kinterp{2,iz0}(k(employed),Kvec(employed));
+        
+        % Update current productivity
+        iz0 = iz1;
+    end
+    
+    K_t = K_t(p.sim_tburn+1:T);
+    z_t = z_t(p.sim_tburn+1:T);
+
+    logKt = log(K_t);
+    
+    b = zeros(2,2);
+    for iz=[1,2]
+        condition = (z_t == iz);
+        icond = find(condition);
+        icond = icond(1:end-1);
+
+        nc = numel(icond);
+        X = [ones(nc,1), logKt(icond)];
+        Y = logKt(icond+1);
+        b(:,iz) = (X'*X) \ (X'*Y);
+    end
 end
